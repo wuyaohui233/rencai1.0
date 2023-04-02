09a9c96803205907d45b4109afb5a38e23da8c27
wuyaohui233 <1115684410@qq.com>
2023-04-02 12:24:42 +0800

    人才认证项目

--- src/main/java/com/yimanb/controller/rencai/RencaiController.java
+++ src/main/java/com/yimanb/controller/rencai/RencaiController.java
@@ -1,22 +1,17 @@
 package com.yimanb.controller.rencai;
 
-import com.alibaba.fastjson.JSON;
 import com.yimanb.common.result.Result;
 import com.yimanb.common.result.ResultCodeEnum;
-import com.yimanb.entity.admin.Admin;
 import com.yimanb.entity.rencai.Expert;
 import com.yimanb.entity.rencai.Unit;
 import com.yimanb.entity.user.User;
 import com.yimanb.service.rencai.RencaiService;
-import com.yimanb.util.token.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/rencai")
@@ -29,12 +24,11 @@ public class RencaiController {
      * 企事业单位申请认证
      * 注册用户 并返回token
      * @param unit 企事业单位实体类
-     * @return
-     * @throws IOException
+     * @return objectResult
      */
     @RequestMapping(value="/unitApprove.do",produces={"application/json;charset=UTF-8"})
     @ResponseBody
-    public Result<Object> unitApprove(Unit unit, User user) throws IOException {
+    public Result<Object> unitApprove(Unit unit, User user){
         //添加企事业单位申请记录
         Result<Object> objectResult;
         try{
@@ -49,12 +43,11 @@ public class RencaiController {
      * 外籍专家申请认证
      * 注册用户 并返回token
      * @param expert 专家实体类
-     * @return
-     * @throws IOException
+     * @return objectResult
      */
     @RequestMapping(value="/expertApprove.do",produces={"application/json;charset=UTF-8"})
     @ResponseBody
-    public Result<Object> expertApprove(Expert expert, User user) throws IOException {
+    public Result<Object> expertApprove(Expert expert, User user){
         //添加企事业单位申请记录
         Result<Object> objectResult;
         try{
@@ -68,12 +61,11 @@ public class RencaiController {
     /**
      * 企事业单位申请查看专家信息
      * @param token 外企专家token
-     * @return
-     * @throws IOException
+     * @return objectResult
      */
     @RequestMapping(value="/unitApply.do",produces={"application/json;charset=UTF-8"})
     @ResponseBody
-    public Result<Object> unitApply(String token,Integer eid) throws IOException {
+    public Result<Object> unitApply(String token,Integer eid){
         //添加企事业单位申请记录
         return null;
     }
